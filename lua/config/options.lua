c04c6f95aab0bd3773c7c15e90bab065ca8e78c7
Deonte Horton <dhorton@vectorworks.net>
2025-08-25 15:05:19 -0500

    Add tab spacing to the options file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.tabstop = 4 -- Set tab width to 4 spaces
+vim.o.shiftwidth = 4 -- Set auto-indent and shift commands to 4 spaces
+vim.o.expandtab = true -- Use spaces instead of tabs when pressing Tab
+vim.o.softtabstop = 4 -- Make Tab and Backspace behave like 4 spaces
