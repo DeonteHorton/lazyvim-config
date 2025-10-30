5e1561a47b87f505eb34d9fa958908de4bc4c070
Deonte Horton <dhorton@vectorworks.net>
2025-10-29 22:31:40 -0500

    Update of lazyvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,4 @@ vim.o.tabstop = 4 -- Set tab width to 4 spaces
 vim.o.shiftwidth = 4 -- Set auto-indent and shift commands to 4 spaces
 vim.o.expandtab = true -- Use spaces instead of tabs when pressing Tab
 vim.o.softtabstop = 4 -- Make Tab and Backspace behave like 4 spaces
+vim.g.autoformat = false -- Turn off autoformatting on save
